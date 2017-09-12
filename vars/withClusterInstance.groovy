272773cfdd9319f7f680743c3d18c3f576a63584
robertschimpfle <robert.schimpfle@comquent.de>
2017-09-12 14:19:33 +0200

    Update withClusterInstance.groovy

--- vars/withClusterInstance.groovy
+++ vars/withClusterInstance.groovy
@@ -1,10 +1,12 @@
 
+@Grab('com.amazonaws:aws-java-sdk')
+
 def call(params = null, body) {
 def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     
-    echo "Working on ${params}
+    echo "Working on ${params}"
     
     body()
     
