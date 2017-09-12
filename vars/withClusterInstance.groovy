ccd3d2d8d4daa9fe90511eab995f52c01f351675
robertschimpfle <robert.schimpfle@comquent.de>
2017-09-12 16:26:14 +0200

    Update withClusterInstance.groovy

--- vars/withClusterInstance.groovy
+++ vars/withClusterInstance.groovy
@@ -1,6 +1,6 @@
 @GrabResolver(name='maven-central', root='https://repo1.maven.org/maven2')
 @Grab('com.amazonaws:aws-java-sdk')
-import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.*
 
 def call(params = null, body) {
 def config = [:]
@@ -8,6 +8,14 @@ def config = [:]
     body.delegate = config
     
     echo "Working on ${params}"
+    AmazonEC2Client ec2Client = AmazonEC2ClientBuilder.defaultClient()
+    RunInstancesRequest runInstancesRequest = new RunInstancesRequest()
+    runInstancesRequest.withImageId('ami-9877a5f7').withInstanceType('m1.small')
+        .withMinCount(1).withMaxCount(1).
+        .withKeyName('Jenkins Training')
+        .withSecurityGroups('Jenkins Master')
+    RunInstancesResult result = ec2Client.runInstances(runInstancesRequest)
+    
     
     body()
     
