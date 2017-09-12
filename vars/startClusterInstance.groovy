f1c894022a1424399c690a84b5953203e0931914
robertschimpfle <robert.schimpfle@comquent.de>
2017-09-12 12:30:57 +0200

    Update startClusterInstance.groovy

--- vars/startClusterInstance.groovy
+++ vars/startClusterInstance.groovy
@@ -1,7 +1,7 @@
 
-/*def call(String name = 'human') {
+def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     echo "Hello."
 }
-*/
+
