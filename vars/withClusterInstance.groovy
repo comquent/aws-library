fd0f5d872ac13ea04ebd8bebb995e6cad6325479
robertschimpfle <robert.schimpfle@comquent.de>
2017-09-12 15:02:41 +0200

    Update withClusterInstance.groovy

--- vars/withClusterInstance.groovy
+++ vars/withClusterInstance.groovy
@@ -1,5 +1,5 @@
 
-@GrabResolver(name='maven-central', root='http://search.maaaaven.org/')
+@GrabResolver(name='maven-central', root='https://repo1.maven.org/maven2')
 @Grab('com.amazonaws:aws-java-sdk')
 import com.amazonaws.services.s3.AmazonS3Client
 
