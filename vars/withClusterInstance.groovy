7ba92c283de087e3b698c588800ea508fc1d8406
robertschimpfle <robert.schimpfle@comquent.de>
2017-09-13 14:25:32 +0200

    Update withClusterInstance.groovy

--- vars/withClusterInstance.groovy
+++ vars/withClusterInstance.groovy
@@ -1,7 +1,6 @@
-// @GrabResolver(name='maven-central', root='https://repo1.maven.org/maven2')
-
-
-//@Grab('org.jenkins-ci.plugins:jackson2-api:2.5.4')
+@GrabResolver(name='repo.jenkins-ci.org', root ='https://repo.jenkins-ci.org/public/')
+@GrabResolver(name='maven-central', root='https://repo1.maven.org/maven2')
+@Grab('org.jenkins-ci.plugins:jackson2-api:2.5.4')
 @Grab('com.amazonaws:aws-java-sdk:1.11.119')
 @GrabExclude(group = 'commons-codec', module = 'commons-codec')
 @GrabExclude(group = 'com.fasterxml.jackson.core', module = 'jackson-databind')
