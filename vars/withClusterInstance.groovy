08e921b48877eca57a50acc56c45eec57484ac2b
robertschimpfle <robert.schimpfle@comquent.de>
2017-09-13 14:43:55 +0200

    Update withClusterInstance.groovy

--- vars/withClusterInstance.groovy
+++ vars/withClusterInstance.groovy
@@ -8,14 +8,6 @@ def config = [:]
     def instance = new EC2Instance()
     
     echo "Working on ${params}"
-    AmazonEC2Client ec2Client = AmazonEC2ClientBuilder.defaultClient()
-    RunInstancesRequest runInstancesRequest = new RunInstancesRequest()
-    runInstancesRequest.withImageId('ami-9877a5f7').withInstanceType('m1.small')
-        .withMinCount(1).withMaxCount(1)
-        .withKeyName('Jenkins Training')
-        .withSecurityGroups('Jenkins Master')
-    RunInstancesResult result = ec2Client.runInstances(runInstancesRequest)
-    
     
     body()
     
