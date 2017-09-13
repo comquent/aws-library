350b811b8fc9d366276496c0d144f50e1ff2d9c4
robertschimpfle <robert.schimpfle@comquent.de>
2017-09-13 14:34:53 +0200

    Update withClusterInstance.groovy

--- vars/withClusterInstance.groovy
+++ vars/withClusterInstance.groovy
@@ -1,18 +1,11 @@
-/*@GrabResolver(name='repo.jenkins-ci.org', root ='https://repo.jenkins-ci.org/public/')
-@GrabResolver(name='maven-central', root='https://repo1.maven.org/maven2')
-@Grab('org.jenkins-ci.plugins:jackson2-api:2.5.4')
-@Grab('com.amazonaws:aws-java-sdk:1.11.119')
-@GrabExclude(group = 'commons-codec', module = 'commons-codec')
-@GrabExclude(group = 'com.fasterxml.jackson.core', module = 'jackson-databind')
-import com.amazonaws.services.ec2.AmazonEC2Client
-import com.amazonaws.services.ec2.AmazonEC2ClientBuilder
-import com.amazonaws.services.ec2.model.RunInstancesRequest
-import com.amazonaws.services.ec2.model.RunInstancesResult
+import de.comquent.awslibrary.EC2Instance
 
 def call(params = null, body) {
 def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
+
+    def instance = new EC2Instance()
     
     echo "Working on ${params}"
     AmazonEC2Client ec2Client = AmazonEC2ClientBuilder.defaultClient()
@@ -26,6 +19,4 @@ def config = [:]
     
     body()
     
-    
 }
-*/
