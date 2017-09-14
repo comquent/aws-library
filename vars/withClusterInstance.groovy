0704635238e76af375efea5289c9f8b7d725fc94
Robert Schimpfle <robert.schimpfle@comquent.de>
2017-09-14 15:40:22 +0200

    Work on credentials

--- vars/withClusterInstance.groovy
+++ vars/withClusterInstance.groovy
@@ -16,6 +16,7 @@ def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
 
+    echo params
     echo "Credentials: ${params.credentials}"
 
     withCredentials([usernamePassword(credentialsId: params.credentials, passwordVariable: 'accessKey', usernameVariable: 'secretAccessKey')]) {
