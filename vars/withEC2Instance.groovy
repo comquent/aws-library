f8289d30f9b6b73215d7423536f9873f6da8999b
Robert Schimpfle <robert.schimpfle@comquent.de>
2017-09-15 18:26:49 +0200

    refactor

--- vars/withEC2Instance.groovy
+++ vars/withEC2Instance.groovy
@@ -107,7 +107,7 @@ def terminateEC2Instance(instanceId) {
  */
 def call(params = null, body) {
     def config = [:]
-    body.resolveStrategy = Closure.OWNER_FIRST
+    body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
 
     // Make methods in closure available
