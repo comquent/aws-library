ce105a6049d21599d17dc705e474e60f56293a0f
robertschimpfle <robert.schimpfle@comquent.de>
2017-09-21 16:00:43 +0200

    Update withAWSCredentials.groovy

--- vars/withAWSCredentials.groovy
+++ vars/withAWSCredentials.groovy
@@ -4,15 +4,10 @@ def call(params = null, body) {
     def config = [:]
     body.resolveStrategy = Closure.OWNER_FIRST
     body.delegate = config
-    body.hello = this.&hello
 
     withCredentials([
         usernamePassword(credentialsId: params.credentials, usernameVariable: 'accessKey', passwordVariable: 'secretAccessKey')
     ]) {
         body()
     }
-}
-
-def hello = {
-    println "HELLO"
 }
