75e7082aeeeaa874740850b7a44227427f5dd971
robertschimpfle <robert.schimpfle@comquent.de>
2017-09-21 14:20:32 +0200

    Update withAWSCredentials.groovy

--- vars/withAWSCredentials.groovy
+++ vars/withAWSCredentials.groovy
@@ -2,14 +2,12 @@ import de.comquent.awslibrary.EC2Instance
 
 def call(params = null, body) {
     def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.resolveStrategy = Closure.OWNER_FIRST
     body.delegate = config
 
     withCredentials([
         usernamePassword(credentialsId: params.credentials, usernameVariable: 'accessKey', passwordVariable: 'secretAccessKey')
     ]) {
-        def helloObject = new EC2Instance()
-        body.hello = helloObject.&hello
         body()
     }
 }
