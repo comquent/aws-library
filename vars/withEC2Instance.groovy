c79fa6d07cca089dde25c5b6b9edd9da82f119b9
robertschimpfle <robert.schimpfle@comquent.de>
2017-10-07 22:43:59 +0200

    Update withEC2Instance.groovy

--- vars/withEC2Instance.groovy
+++ vars/withEC2Instance.groovy
@@ -9,9 +9,9 @@ def call(params = null, body) {
     def instanceId = createEC2Instance()
     body.INSTANCE_ID = instanceId
     
-    body.EC2Instance = new Object()
-    body.EC2Instance.metaClass.mixin Serializable
-    body.EC2Instance.terminate = this.&t
+    body.EEC2Instance = new Object()
+    body.EEC2Instance.metaClass.mixin Serializable
+    body.EEC2Instance.terminate = this.&t
 
     body.PUBLIC_DNS_NAME = waitOnEC2Instance(instanceId)
 
