0c0f926e49339b2bae6c8cc39d59bbd99595db1a
robertschimpfle <robert.schimpfle@comquent.de>
2017-10-22 20:55:31 +0200

    Update withS3Instance.groovy

--- vars/withS3Instance.groovy
+++ vars/withS3Instance.groovy
@@ -61,10 +61,10 @@ def listFiles(storageName) {
 
 @NonCPS
 def downloadFile(storageName, fileName) {
-    stream = getS3Client().getObject(storageName, fileName).getObjectContent();
-    fos = new FileOutputStream(new File(fileName));
+    def stream = getS3Client().getObject(storageName, fileName).getObjectContent()
+    def fos = new FileOutputStream(new File(fileName))
     byte[] read_buf = new byte[1024];
-    read_len = 0;
+    def read_len = 0;
     while ((read_len = stream.read(read_buf)) > 0) {
         fos.write(read_buf, 0, read_len);
     }
