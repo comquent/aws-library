7218d6f91c926fec43c612dda451299e183d176d
robertschimpfle <robert.schimpfle@comquent.de>
2017-10-23 17:47:10 +0200

    Update withS3Instance.groovy

--- vars/withS3Instance.groovy
+++ vars/withS3Instance.groovy
@@ -4,6 +4,8 @@ import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 
+import hudson.FilePath
+
 
 @NonCPS
 def getS3Client() {
